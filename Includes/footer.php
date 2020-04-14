6aab7892ab8328a688f219227f1323e0d212b1c0
D.Jesintha <48330710+DJaishintha@users.noreply.github.com>
2020-04-14 14:43:14 +0530

    Add files via upload

---
+++ Includes/footer.php
@@ -0,0 +1,8 @@
+	<footer style="height: 90px; padding:5px; "class="text-center navbar-dark bg-dark">
+	<h3 class="text-white">follow us at :</h3>
+<a href="https://www.facebook.com/maternalmentalhealthmom/" class="fa fa-facebook pr-5 text-white"></a>
+<a href="https://twitter.com/hashtag/maternalhealth?f=live" class="fa fa-twitter pr-5 text-white"></a>
+<a href="https://www.instagram.com/explore/tags/maternalhealth/" class="fa fa-instagram pr-5 text-white"></a>
+<a href="https://www.youtube.com/channel/UCve35SaPzPzCCRQ4BDP2chQ/videos" class="fa fa-youtube pr-5 text-white"></a>
+</footer>
+
---
+++ Includes/header.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>header</title>
+
+  <link rel="stylesheet" type="text/css" href="bootstrap.css">
+  <link rel="stylesheet" type="text/css" href="fafacode.min.css">
+  <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+
+
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <img src="assets/logo.png" width="200" height="80">
+  <a class="navbar-brand" href="index.php">MATERNAL HEALTH CARE</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav ml-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="index.php"><i class="fa fa-fw fa-home"></i>HOME <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="healthnew.php">HEALTH TOPICS</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="region.php">AREA</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="contactdoc.php">CONTACT DOCTORS</a>
+       <li class="nav-item">
+        <a class="nav-link" href="about.php">ABOUT US</a>
+      </li>
+      </li>
+   </div>
+    </ul>
+
+  </div>
+</nav>
+
+
+</body>
+</html>
